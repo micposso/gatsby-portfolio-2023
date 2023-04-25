4475023f43608435eb31caf9d6d2c66b11daca41
micposso <micposso@gmail.com>
2023-04-25 16:35:19 -0400

    add vector

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,5 +27,13 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/ // See below to configure properly
+        }
+      }
+    }
   ],
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
+import Monster from '../assets/monster-1.svg';
+
 import HALO from "vanta/dist/vanta.halo.min";
 
 const Home = ({ links }) => {
@@ -28,13 +30,27 @@ const Home = ({ links }) => {
       <div className="overlay"></div>
 
       <Card style={{ width: "35rem" }}>
+        <div className="vector-container">
+          <Monster />
+        </div>
       <div className="name-card text-center">michael posso</div>
+      <div className="line-container">
+        <div className="card-vertical-line">
+        </div>
+      </div>
       <Card.Text  className="text-center">
             Enginering Manager | Tech Educator | UI Developer
           </Card.Text>
         <Card.Body>
           <Card.Title>I build and manage development teams that scale.</Card.Title>
          </Card.Body>
+         <div className="line-container">
+          <div className="card-vertical-line"></div>
+          <div className="vertical-text">
+            scrolldown
+          </div>
+        </div>
+
       </Card>
       </div>
 
