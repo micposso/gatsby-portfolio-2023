c601e382dbf92551e5206488fd8a61e9d9191296
micposso <micposso@gmail.com>
2023-05-24 23:28:46 -0400

    add descriptions

--- src/components/skills.js
+++ src/components/skills.js
@@ -6,6 +6,8 @@ import { SiTypescript } from "react-icons/si";
 import { SiTailwindcss } from "react-icons/si";
 import { SiD3Dotjs } from "react-icons/si";
 import { DiSass } from "react-icons/di";
+import { BsWordpress } from "react-icons/bs";
+import { SiMysql } from "react-icons/si";
 
 import { css } from "@emotion/react";
 
@@ -27,17 +29,17 @@ const fadeIn = css`
 `;
 
 const monsterIn = css`
-@keyframes monster-in {
-  from {
-    right: -25%;
+  @keyframes monster-in {
+    from {
+      right: -25%;
+    }
+    to {
+      right: -10%;
+    }
   }
-  to {
-    right: -10%;
-  }
-}
 
-animation: monster-in 1s ease-out;
-`
+  animation: monster-in 1s ease-out;
+`;
 
 const Skills = () => {
   const [vantaEffect, setVantaEffect] = useState(null);
@@ -102,17 +104,19 @@ const Skills = () => {
           </Col>
           <Col xs={12} md={12} lg={8}>
             <p>
-              In hac habitasse platea dictumst. Phasellus tristique lacus quis
-              eros cursus faucibus. Proin odio velit, pulvinar at rhoncus non,
-              tempor ac erat. Fusce congue augue vitae volutpat scelerisque.
-              Aliquam et vehicula tortor. Curabitur in volutpat risus, eget
-              mollis leo. Donec non placerat urna. Pellentesque tincidunt
-              venenatis imperdiet. Fusce sodales enim eget ipsum commodo
-              eleifend.
+              <span className="text-uppercase">
+                With a full dozen years of navigating the web development seas
+              </span>
+              , I've honed my skills in HTML, CSS, JavaScript, and React for
+              responsive front-end designs. I adeptly use Node.js, PHP, SQL,
+              NoSQL, and Docker, integrating project management and Scrum
+              methodologies into my ship. My expertise also spans version
+              control/Git, debugging, UX/UI principles, SEO optimization, and
+              email marketing.
             </p>
           </Col>
         </Row>
-        <Accordion defaultActiveKey="0">
+        <Accordion>
           <Accordion.Item eventKey="0">
             <Accordion.Header>
               <h3>Web Development</h3>
@@ -122,24 +126,24 @@ const Skills = () => {
                 <Row>
                   <Col>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
+                      I employ WordPress and React to build responsive,
+                      enterprise-level websites for Fortune 500 companies.
+                      Leveraging my expertise in these tools, I transform
+                      complex requirements into high-performance digital
+                      experiences, ensuring cross-device compatibility, seamless
+                      user journeys, robust security, and high scalability to
+                      drive business growth.
                     </p>
                   </Col>
                   <Col>
+                    <BsWordpress className="tech-icons" />
                     <IoLogoJavascript className="tech-icons" />
+                    <SiTypescript className="tech-icons" />
+                    <DiReact className="tech-icons" />
                     <SiTailwindcss className="tech-icons" />
                     <DiSass className="tech-icons" />
-                    <DiReact className="tech-icons" />
-                    <SiTypescript className="tech-icons" />
                     <SiD3Dotjs className="tech-icons" />
-
+                    <SiMysql className="tech-icons" />
                   </Col>
                 </Row>
               </Container>
@@ -147,17 +151,20 @@ const Skills = () => {
           </Accordion.Item>
           <Accordion.Item eventKey="1">
             <Accordion.Header>
-              <h3>Technical Project Management</h3>
+              <h3>Agile Project Management</h3>
             </Accordion.Header>
             <Accordion.Body>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                As a web developer and team leader, I excel in establishing
+                high-performance development teams. Utilizing Agile and Scrum
+                methodologies, we promote continuous improvement, fueled by
+                tools like Jira for task tracking and clear communication. This
+                framework optimizes development cycles, supports adaptability,
+                and ensures quality deliverables. My leadership revolves around
+                effective communication, problem-solving, and fostering
+                innovation. We surpass our objectives through this comprehensive
+                approach to project management, delivering top-tier, innovative
+                digital solutions.
               </p>
             </Accordion.Body>
           </Accordion.Item>
@@ -167,13 +174,16 @@ const Skills = () => {
             </Accordion.Header>
             <Accordion.Body>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                I leverage Docker and advanced Git workflows to establish
+                continuous delivery pipelines for enterprise-level websites and
+                applications. Docker streamlines development by creating a
+                consistent, isolated environment, enhancing scalability and
+                deployment speed. Git, with its advanced workflows, ensures
+                smooth version control, facilitating collaboration. By
+                integrating these tools, I automate and optimize delivery
+                processes, enabling frequent, reliable releases, reducing the
+                time to market, and increasing the ability to respond to
+                changes, thus driving operational efficiency and business value.
               </p>
             </Accordion.Body>
           </Accordion.Item>
@@ -183,26 +193,46 @@ const Skills = () => {
             </Accordion.Header>
             <Accordion.Body>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                I integrate my SEO and email marketing acumen into
+                enterprise-level website creation, aligning digital strategies
+                with business requirements. My SEO knowledge ensures optimized
+                site structure, load speed, and content for better search engine
+                rankings. Leveraging email marketing, I facilitate user
+                engagement, retention, and conversion. This amalgamation of
+                technical and digital marketing skills allows me to build
+                robust, high-performing websites that boost online visibility,
+                drive customer interaction, and ultimately contribute to
+                business growth.
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>
+              <h3>Unity Development</h3>
+            </Accordion.Header>
+            <Accordion.Body>
+              <p>
+                I leverage Unity and C# to create immersive VR, AR, and mixed
+                reality experiences for educational and training simulations. By
+                combining Unity's powerful development platform with C#'s
+                flexibility, I build interactive environments that enhance
+                learning. I utilize Blender for crafting intricate 3D models,
+                ensuring visually captivating and realistic simulations.
               </p>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
-        <Row></Row>
       </Container>
 
-      <MonsterSkills className="monster-skills" ref={ref}
-        css={isVisible ? monsterIn : null} />
+      <MonsterSkills
+        className="monster-skills"
+        ref={ref}
+        css={isVisible ? monsterIn : null}
+      />
     </div>
   );
 };
 
-export const Head = () => <title>About Me</title>;
+export const Head = () => <title>Web Development</title>;
 
 export default Skills;
--- src/components/teaching.js
+++ src/components/teaching.js
@@ -1,13 +1,66 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Link } from "gatsby";
 import { Container, Row, Col, Accordion } from "react-bootstrap";
 import MonsterTeaching from "../assets/monster-4.svg";
+import { css } from "@emotion/react";
 
 import BIRDS from "vanta/dist/vanta.birds.min";
 
-const Teaching = ({ links }) => {
+const fadeIn = css`
+  @keyframes fade-in {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  animation: fade-in 1s ease-out;
+`;
+
+const monsterIn = css`
+  @keyframes monster-in {
+    from {
+      right: -25%;
+    }
+    to {
+      right: -10%;
+    }
+  }
+
+  animation: monster-in 1s ease-out;
+`;
+
+const Teaching = () => {
   const [vantaEffect, setVantaEffect] = useState(null);
   const myRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target);
+        }
+      },
+      {
+        rootMargin: "0px",
+        threshold: 0.1,
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
 
   useEffect(() => {
     if (!vantaEffect) {
@@ -37,89 +90,82 @@ const Teaching = ({ links }) => {
           </Col>
           <Col xs={12} md={12} lg={8}>
             <p>
-              In hac habitasse platea dictumst. Phasellus tristique lacus quis
-              eros cursus faucibus. Proin odio velit, pulvinar at rhoncus non,
-              tempor ac erat. Fusce congue augue vitae volutpat scelerisque.
-              Aliquam et vehicula tortor. Curabitur in volutpat risus, eget
-              mollis leo. Donec non placerat urna. Pellentesque tincidunt
-              venenatis imperdiet. Fusce sodales enim eget ipsum commodo
-              eleifend.
+              With 7 years of education experience, I chart courses in the vast
+              sea of tech education for eager learners and college students. My
+              compass? A meticulously crafted curriculum filled with fundamental
+              skills and the latest technological advancements. My teachings,
+              filled with sailor wisdom, set sail in the minds of learners,
+              equipping them to confidently navigate the ever-changing digital
+              seascape.
             </p>
           </Col>
         </Row>
-        <Accordion defaultActiveKey="0">
+        <Accordion>
           <Accordion.Item eventKey="0">
-            <Accordion.Header>
-              <h3>Technology Curriculum Development</h3>
-            </Accordion.Header>
-            <Accordion.Body>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
             <Accordion.Header>
               <h3>Fashion Institute of Technology</h3>
             </Accordion.Header>
             <Accordion.Body>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                As an instructor in the Creative Technology department at FIT, I
+                lead an array of courses on cutting-edge topics. In UX/UI
+                Design, I guide students in leveraging virtual reality to create
+                immersive experiences. For Web Development, I teach the nuances
+                of WordPress and Webflow, empowering students to build and
+                manage modern websites. Lastly, in my 3D Brand Design course,
+                students learn to use Blender and Unity for creating compelling
+                brand assets. My curriculum blends theory with hands-on
+                practice, preparing students to excel in these dynamic fields.
               </p>
             </Accordion.Body>
           </Accordion.Item>
-          <Accordion.Item eventKey="2">
+          <Accordion.Item eventKey="1">
             <Accordion.Header>
               <h3>New York Institute of Technology</h3>
             </Accordion.Header>
             <Accordion.Body>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                As an adjunct professor in the Master's program for UX/UI, I
+                orchestrate in-depth courses marrying coding, design, and user
+                interface principles. Through instructive modules on HTML, CSS,
+                JavaScript, and React, I empower students to build visually
+                captivating, user-friendly digital interfaces. I also teach Git
+                and GitHub, ensuring students master version control and
+                collaborative development practices. My curriculum fuses theory
+                with hands-on application, fostering a deep understanding of
+                user-centric design and modern development workflows. My mission
+                is to cultivate future UX/UI professionals who excel in creating
+                engaging, seamless digital experiences.
               </p>
             </Accordion.Body>
           </Accordion.Item>
-          <Accordion.Item eventKey="3">
+          <Accordion.Item eventKey="2">
             <Accordion.Header>
-              <h3>Miami University</h3>
+              <h3>Curriculum Development</h3>
             </Accordion.Header>
             <Accordion.Body>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                With my extensive web development expertise, I harness
+                technology and industry knowledge to craft curriculum for high
+                school, college, and graduate courses. I dissect intricate
+                concepts into digestible lessons, integrating emerging tech
+                trends to keep the content current. My curriculum aims to bridge
+                the gap between theory and practice, offering students hands-on
+                experience and a profound understanding of real-world
+                applications. I'm dedicated to fostering an innovative learning
+                environment that prepares students for future tech careers.
               </p>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
         <Row></Row>
       </Container>
-      <MonsterTeaching className="monster-teaching" />
+      <MonsterTeaching className="monster-teaching" ref={ref}
+        css={isVisible ? monsterIn : null}/>
     </div>
   );
 };
 
-export const Head = () => <title>About Me</title>;
+export const Head = () => <title>Teaching Experience</title>;
 
 export default Teaching;
